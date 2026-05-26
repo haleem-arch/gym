edc868522386f5ec64351d4af3ce7db6e426ca20
strideriteclub-cell <strideriteclub@gmail.com>
2026-05-26 11:15:46 +0300

    fix: import useEffect in DietHome

--- src/pages/DietHome.tsx
+++ src/pages/DietHome.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDiet } from '../hooks/useDiet';
 import { MacroProgressBar } from '../components/MacroProgressBar';
