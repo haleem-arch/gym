14465ccf25f75992c3b7c44569e7ee8aa709894c
strideriteclub-cell <strideriteclub@gmail.com>
2026-06-11 11:08:05 +0300

    fix: simplify uuid regex for safer custom exercise resolution

--- src/pages/WorkoutTracker.tsx
+++ src/pages/WorkoutTracker.tsx
@@ -112,7 +112,7 @@ const WorkoutTracker = () => {
       // Resolve custom/temporary exercise IDs
       const resolvedExercises = await Promise.all(
         workout.exercises.map(async (ex) => {
-          const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+          const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
           if (uuidRegex.test(ex.id)) {
             return ex;
           }
